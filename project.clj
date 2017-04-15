6a2f0b0674da0a8cc2db07ed9a6f46e168c3fd5e
La Monade <iomonad@riseup.net>
2017-04-15 20:26:00 +0200

    Added irc library, changed license and added some foo test.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject gcwl "0.1.0"
   :description "Gcwl is my personnal irc bot, written in Clojure"
   :url "https://github.com/iomonad/gcwl"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :license {:name "Do What the Fuck You Want to Public License"
+            :url "http://www.wtfpl.net/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [irclj "0.5.0-alpha4"]])
--- test/gcwl/core_test.clj
+++ test/gcwl/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [gcwl.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest some-foo-test
+  (testing "Make sure test suite works"
+    (is (= 1 1))))
