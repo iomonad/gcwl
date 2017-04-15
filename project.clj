d83f5ae466ea562ae886407b26a9eb1ee475cce3
La Monade <iomonad@riseup.net>
2017-04-15 20:15:34 +0200

    Project initialisation

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject gcwl "0.1.0"
+  :description "Gcwl is my personnal irc bot, written in Clojure"
+  :url "https://github.com/iomonad/gcwl"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
