d49c796180e8f7ddd722ad347680441774a04252
La Monade <iomonad@riseup.net>
2017-04-16 23:34:28 +0200

    Moving to irclj library

--- src/gcwl/config.clj
+++ src/gcwl/config.clj
@@ -1,7 +1,7 @@
 ;; Filename: config.clj
 ;; Copyright (c) 2008-2017 Clement Trösa <iomonad@riseup.net>
 ;; 
-;; Last-Updated: 04/16/2017 Sunday 09:32:01
+;; Last-Updated: 04/16/2017 Sunday 23:34:04
 ;; Description: Configuration namespaces for the bot
 
 (ns gcwl.config
@@ -15,13 +15,3 @@
 (defn get-conf []
   "Parse json from configuration file"
   (json/parse-string (slurp conf-path) true))
-
-(def settings
-  {:nick  "gcwl"
-   :name  "gcwl"
-   :sname "gcwl"
-   :rname "gcwl"
-   :server "irc.freenode.net"
-   :port 6667
-   :channels "#bots"
-  })
--- src/gcwl/core.clj
+++ src/gcwl/core.clj
@@ -1,13 +1,12 @@
 ;; Filename: core.clj
 ;; Copyright (c) 2008-2017 Clement Trösa <iomonad@riseup.net>
 ;; 
-;; Last-Updated: 04/16/2017 Sunday 22:24:15
+;; Last-Updated: 04/16/2017 Sunday 23:33:37
 ;; Description: Main namespace of the program
 
 (ns gcwl.core
   "Main namespace of the program"
   (:require [gcwl.config   :as config]
-            [gcwl.irc      :as irc]
             [clojure.string :as string]
             [cheshire.core :as json]))
 
