fd308bd666b065d1ab9ebdfc77482455d8c51bc2
La Monade <iomonad@riseup.net>
2017-05-06 22:32:48 +0200

    Removed useless config namespace

--- src/salmon/core.clj
+++ src/salmon/core.clj
@@ -1,16 +1,14 @@
 ;; Filename: core.clj
 ;; Copyright (c) 2008-2017 Clement Trösa <iomonad@riseup.net>
-;; 
-;; Last-Updated: 05/06/2017 Saturday 22:04:27
+;;
+;; Last-Updated: 05/06/2017 Saturday 22:32:34
 ;; Description: Main namespace of the program
 
 
 (ns salmon.core
   "Main namespace of the program"
-  (:require [salmon.config  :as config]
-            [salmon.bot     :as bot]
-            [salmon.plugins :as plugins]
-            )
+  (:require [salmon.bot     :as bot]
+            [salmon.plugins :as plugins])  
   (:gen-class))
 
 (defn -main []
