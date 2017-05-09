0b4d6ee624a48f1bdc8f5d59b1e8918d8311401d
La Monade <iomonad@riseup.net>
2017-05-09 22:34:45 +0200

    Fixed parsing, Multiline need to be fixed.

--- src/salmon/plugins/man.clj
+++ src/salmon/plugins/man.clj
@@ -1,22 +1,24 @@
 ;; Filename: man.clj
 ;; Copyright (c) 2008-2017 Clement Trösa <iomonad@riseup.net>
 ;; 
-;; Last-Updated: 05/09/2017 Tuesday 21:25:39
+;; Last-Updated: 05/09/2017 Tuesday 22:34:35
 ;; Description: 
 
 (ns salmon.plugins.man
   (:require [salmon.parse :as parse]
             [clojure.string :as str]))
 
+
+
 (defn fn-man [irc message]
   (let [plugins (:plugins @irc)]
     (if-let [[command _] (parse/extract-word message)]
-      (or (let [p (->> plugins (map #(get-in % [:desc command])) (remove nil?)
-                       first)]
-            (str p)
-            (str "No manual entry for \"" command "\".")))
+      (or (->> plugins (map #(get % :desc)) (remove nil?)
+                                        ;first ;TODO: Predicate need to bee fixed
+               ))
       (str "What manual page do you want? Please use help command."
            ))))
+
 
 (def plugin {:name "man"
              :desc "Show command documention"
