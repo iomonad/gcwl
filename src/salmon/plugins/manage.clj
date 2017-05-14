a668046b8fa2138229a1341fed02165ffa5abb47
La Monade <iomonad@riseup.net>
2017-05-14 11:46:21 +0200

    Revert "Updated manage plugin."
    This reverts commit 4ae410e1242558d46e9ae8f60cdd2d38499c3831.

--- src/salmon/plugins/manage.clj
+++ src/salmon/plugins/manage.clj
@@ -1,7 +1,7 @@
 ;; Filename: manage.clj
 ;; Copyright (c) 2008-2017 Clement Trösa <iomonad@riseup.net>
 ;; 
-;; Last-Updated: 05/11/2017 Thursday 22:11:28
+;; Last-Updated: 05/11/2017 Thursday 21:54:57
 ;; Description: Manage bot state
 
 (ns salmon.plugins.manage
@@ -9,14 +9,9 @@
             [clojure.string :as str]))
 
 (defn fn-manage [irc message]
-  (let [nick (get message :nick)
-        args (get message :params)]
-    (if (parse/admin? nick)
-      (let [cmd (nth args 2)]
-        (case cmd
-          "stop" (System/exit 0) 
-          (format "Task `%s` not found."))) ; Default
-      (str "Error, you must be an administrator."))))
+  (let [admin (get message :nick)
+        ]
+    (str message)))
 
 (def plugin {:name "manage"
              :desc {"manage" "Maintenance commands to manage bot state"}
